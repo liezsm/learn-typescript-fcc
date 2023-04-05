67714cda5b380c080c87afa3073caeef640a6b9a
liezsm <liez.dev@gmail.com>
2023-04-06 06:17:32 +0800

    functions

--- myfunctions.js
+++ myfunctions.js
@@ -17,3 +17,31 @@ var loginUser = function (name, email, isPaid) {
     if (isPaid === void 0) { isPaid = false; }
 };
 loginUser("h", "h@h.com");
+// -better way to write function
+// reminder: better way is to put also a type in return as well as the parameter
+function getValue(num) {
+    if (num > 5) {
+        return true;
+    }
+    // return "200 OK"
+    return false;
+}
+// -in arrow function
+var getHello = function (s) {
+    return "";
+};
+// const heros = ['ironman', 'spiderman']
+var heros = [1, 2, 3];
+// -lesson: so typescript also automatically infered the type when also using higher order functions like this
+heros.map(function (hero) {
+    return "hero is ".concat(hero);
+});
+// -functions that doesnt return, put a void as type
+function consoleError(errmsg) {
+    console.log(errmsg);
+}
+// -there are functions that never returns a value (void returns nothing)
+// -best case uses is when throwing error or teminates execution of program
+function handleError(errmsg) {
+    throw new Error(errmsg);
+}
--- myfunctions.ts
+++ myfunctions.ts
@@ -22,4 +22,40 @@ let loginUser = (name: string, email: string, isPaid: boolean = false) => {};
 
 loginUser("h", "h@h.com");
 
+// -better way to write function
+// reminder: better way is to put also a type in return as well as the parameter
+
+function getValue(num: number): boolean {
+  if (num > 5) {
+    return true;
+  }
+
+  // return "200 OK"
+  return false;
+}
+
+// -in arrow function
+
+const getHello = (s: string): string => {
+  return "";
+};
+
+// const heros = ['ironman', 'spiderman']
+const heros = [1, 2, 3];
+
+// -lesson: so typescript also automatically infered the type when also using higher order functions like this
+
+heros.map((hero) => {
+  return `hero is ${hero}`;
+});
+// -functions that doesnt return, put a void as type
+function consoleError(errmsg: string): void {
+  console.log(errmsg);
+}
+
+// -there are functions that never returns a value (void returns nothing)
+// -best case uses is when throwing error or teminates execution of program
+function handleError(errmsg: string): void {
+  throw new Error(errmsg);
+}
 export {};
