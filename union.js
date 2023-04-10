61602d34197283b8bf61c12b2548768dec20df1e
liezsm <liez.dev@gmail.com>
2023-04-10 15:26:09 +0800

    union lesson 4-10-20

--- union.js
+++ union.js
@@ -10,3 +10,23 @@ hites = {
     username: "admin",
     id: 444
 };
+function getDBId(id) {
+    console.log("DB is is ".concat(id));
+    if (typeof id == "string") {
+        id.toLowerCase();
+    }
+    if (typeof id == "number") {
+        id.toFixed();
+    }
+}
+getDBId(3);
+getDBId("3");
+// using union in array
+// const data: number[] = [1,2,3,"4"]
+// const data: string[] = [1,2,3,"4"]
+// const data: string[] | number[] = [1,2,3,"4"]
+var data = [1, 2, 3, "4"];
+// example
+var seatAllotment;
+// seatAllotment = "test"
+seatAllotment = "aisle";
--- union.ts
+++ union.ts
@@ -26,4 +26,32 @@ hites = {
   id: 444,
 };
 
+function getDBId(id: number | string) {
+  console.log(`DB is is ${id}`);
+
+  if (typeof id == "string") {
+    id.toLowerCase();
+  }
+  if (typeof id == "number") {
+    id.toFixed();
+  }
+}
+
+getDBId(3);
+getDBId("3");
+
+// using union in array
+
+// const data: number[] = [1,2,3,"4"]
+// const data: string[] = [1,2,3,"4"]
+// const data: string[] | number[] = [1,2,3,"4"]
+
+const data: (string | number)[] = [1, 2, 3, "4"];
+
+// example
+
+let seatAllotment: "aisle" | "middle" | "window";
+
+// seatAllotment = "test"
+seatAllotment = "aisle";
 export {};
