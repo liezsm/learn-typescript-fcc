33b2a506d3f7883d7ba1fdd9caab024a59d6c9f5
liezsm <liez.dev@gmail.com>
2023-04-15 11:10:39 +0800

    Interface vs type 04-15-23

---
+++ interface.js
@@ -0,0 +1,23 @@
+var hitesh = {
+    dbID: 22233,
+    email: " h@com",
+    userId: 13343,
+    startTrial: function () {
+        return "trial started";
+    },
+    getCoupon: function (couponname) {
+        return 10;
+    }
+};
+var hitesh2 = {
+    dbID: 22233,
+    email: " h@com",
+    userId: 13343,
+    startTrial: function () {
+        return "trial started";
+    },
+    getCoupon: function (couponname) {
+        return 10;
+    },
+    role: "admin"
+};
--- interface.ts
+++ interface.ts
@@ -7,6 +7,10 @@ interface User {
   getCoupon(couponname: string): number;
 }
 
+interface Admin extends User {
+  role: "admin" | "ta" | "learner";
+}
+
 const hitesh: User = {
   dbID: 22233,
   email: " h@com",
@@ -17,4 +21,43 @@ const hitesh: User = {
   getCoupon: (couponname: "ktacoupokn") => {
     return 10;
   },
+};
+
+const hitesh2: Admin = {
+  dbID: 22233,
+  email: " h@com",
+  userId: 13343,
+  startTrial: () => {
+    return "trial started";
+  },
+  getCoupon: (couponname: "ktacoupokn") => {
+    return 10;
+  },
+  role: "admin",
+};
+
+// -interface vs types
+
+// - types and interface are alike in many ways but the difference is interface can be reopend and can add new properties while type, we cannot.
+
+// -example below
+
+interface Sample {
+  name: string;
+  email: string;
+  id: number;
+}
+
+interface Sample {
+  age: number;
+}
+
+// -if we want to extend using type, we can by using intersection
+
+type UserSample = {
+  name: string;
+};
+
+type User1 = UserSample & {
+  age: number;
 };
