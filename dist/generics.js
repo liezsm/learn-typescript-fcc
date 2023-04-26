20e3c61ff8272f2e1c90c5778459748e54d27f29
liezsm <liez.dev@gmail.com>
2023-04-26 19:28:03 +0800

    lessons on generics

---
+++ dist/generics.js
@@ -0,0 +1,18 @@
+"use strict";
+const score = [];
+const names = [];
+function identityOne(val) {
+    return val;
+}
+function identityTwo(val) {
+    return val;
+}
+function identityThree(val) {
+    return val;
+}
+identityThree(3);
+identityThree("3");
+function identityFour(val) {
+    return val;
+}
+identityFour({});
